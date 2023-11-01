c9e14e35fc0d4baacb97546ccac5d68ff8f7c908
Samir Salman <33979978+samirsalman@users.noreply.github.com>
2023-11-01 22:44:44 +0100

    Feature/mongo handler (#2)
    * mongo handler implementation
    
    * logging and exception in mongo-handler
    
    * requirements
    
    * mongodbfix
    
    * mongodb docker compose
    
    * mongodb fix
    
    * mongo handler data json fix
    
    * node id info
    
    * node param
    
    * remote handler class
    
    * fixed mongohandler node + readme
    
    * fixed import
    
    * env_name as mongo_collection
    
    * Delete Dockerfile
    
    * mongodb become neuropulse

--- src/configs/parse_configs.py
+++ src/configs/parse_configs.py
@@ -1,8 +1,8 @@
 # yaml
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Literal
 from typing_extensions import Annotated
-from attr import dataclass
 import yaml
 
 from src.handlers.handler import Handler
@@ -18,6 +18,16 @@ class FileHandlerConfig:
 
 
 @dataclass
+class MongoHandlerConfig:
+    mongo_host: Annotated[str, "mongo_host"]
+    mongo_collection: Annotated[str, "mongo_collection"]
+    mongo_db: Annotated[str, "mongo_db"]
+    node_id: Annotated[str, "node_id"]
+    mongo_user: Annotated[str, "mongo_user"] = None
+    mongo_password: Annotated[str, "mongo_password"] = None
+
+
+@dataclass
 class NeuroPulseConfig:
     app_logging_level: Annotated[
         Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], "app_logging_level"
@@ -27,7 +37,7 @@ class NeuroPulseConfig:
     environment_name: Annotated[str, "environment_name"]
 
 
-def parse_configs(config_path: str):
+def parse_configs(config_path: str, node: str = None):
     config_path = Path(config_path)
     if not config_path.exists():
         raise FileNotFoundError(f"Config file not found: {config_path}")
@@ -35,12 +45,18 @@ def parse_configs(config_path: str):
     with open(str(config_path), "r") as f:
         config = yaml.safe_load(f)
 
+    environment_name = config.get("environment_name", "default")
     return NeuroPulseConfig(
         app_logging_level=config.get("app_logging_level", "INFO"),
         gpu_monitoring_interval=config.get("gpu_monitoring_interval", 5),
+        environment_name=environment_name,
         handlers=[
-            Handler.get_handler(handler["type"])(**handler.get("config", {}))
+            Handler.get_handler(handler["type"])(
+                **handler.get("config", {}),
+                node_id=node,
+                mongo_collection=environment_name,
+                mongo_db="neuropulse",
+            )
             for handler in config.get("handlers", [])
         ],
-        environment_name=config.get("environment_name", "default"),
     )
---
+++ src/handlers/mongo_handler.py
@@ -0,0 +1,80 @@
+import json
+from typing import List
+from src.data.generic_data import GenericData
+from src.handlers.handler import HandlerMode, RemoteHandler
+from pymongo import MongoClient
+from src.app_logging.logger import logger
+
+
+class MongoHandler(RemoteHandler):
+    def __init__(
+        self,
+        name: str = None,
+        mode: HandlerMode = HandlerMode.APPEND,
+        mongo_host: str = None,
+        mongo_user: str = None,
+        mongo_password: str = None,
+        mongo_db: str = None,
+        mongo_collection: str = None,
+        node_id: str = "node-0",
+        **kwargs,
+    ) -> None:
+        super().__init__(name, mode, node_id)
+        self.user = mongo_user
+        self.password = mongo_password
+        self.mongo_uri = mongo_host
+        self.mongo_db = mongo_db
+        self.mongo_collection = mongo_collection
+        self.client = MongoClient(
+            self.mongo_uri,
+            username=self.user,
+            password=self.password,
+        )
+        try:
+            self.client.server_info()
+        except Exception as e:
+            logger.error(f"Could not connect to mongo: {e}")
+            raise e
+
+        self.db = self.client[self.mongo_db]
+        self.node_id = node_id
+
+    def handle(self, data: List[GenericData]):
+        logger.info(f"Saving {len(data)} data to mongo")
+        _data = []
+        for d in data:
+            d = d.to_json()
+            d["index"] = self.db[self.mongo_collection].count_documents({})
+            d["node_id"] = self.node_id
+            _data.append(d)
+        self.db[self.mongo_collection].insert_many(_data)
+        logger.info(f"Saved {len(data)} data to mongo")
+
+    def close(self):
+        logger.info("Closing mongo connection")
+        self.client.close()
+        logger.info("Closed mongo connection")
+
+    def get_all(self):
+        return self.db[self.mongo_collection].find()
+
+    def get_by_id(self, id: str):
+        return self.db[self.mongo_collection].find_one({"_id": id})
+
+    def get_last(self):
+        return self.db[self.mongo_collection].find().sort([("_id", -1)]).limit(1)
+
+    def get_last_n(self, n: int):
+        return self.db[self.mongo_collection].find().sort([("_id", -1)]).limit(n)
+
+    def get_index(self, index: int):
+        return self.db[self.mongo_collection].find({"index": index})
+
+    def get_by_node_id(self, node_id: str):
+        return self.db[self.mongo_collection].find({"node_id": node_id})
+
+    def get_by_query(self, query: dict):
+        return self.db[self.mongo_collection].find(query)
+
+    def clear(self):
+        self.db[self.mongo_collection].delete_many({})
